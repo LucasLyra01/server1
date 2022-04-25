bae6fdf8cc07a5a12812f0a85fe6691c1c1c8d72
LucasLyra01 <lucaslyra8@gmail.com>
2022-04-25 00:02:42 -0300

    v6

--- src/server.js
+++ src/server.js
@@ -1,26 +1,19 @@
+
 require('dotenv').config({
     path: '.env'
 });
 
 const express = require('express');
 const cors = require('cors');
-// const mongodb = require('./infra/mongodb');
-// const catalogador = require('../robo/index');
 const app = express();
 
-const port = process.env.APP_PORT;
-const hostname = process.env.APP_HOSTNAME;
+const port = process.env.PORT;
+const hostname = process.env.HOSTNAME;
 
 app.use(cors());
-
-const cadastroRoutes = require('./routes/cadastro-routes');
-const cadastroGoogleRoutes = require('./routes/cadastro-google-routes');
-const catalogadosRoutes = require('./routes/catalogados-routes')
-
 app.use(express.urlencoded({
     extended: true
-}))
-
+}));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -38,15 +31,11 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.use('/api/cadastro', cadastroRoutes);
-// app.use('/api/cadastrogoogle', cadastroGoogleRoutes);
-// app.use('/catalogacao', catalogadosRoutes);
-
 app.get('/', (req, res) => {
-    res.json({
+    res.send({
         status: 'ok',
-        message: `O servidor da Elite Wolf está rodando`
-    })
+        message: 'Rodando'
+    });
 });
 
 app.use((req, res, next) => {
@@ -63,5 +52,7 @@ app.use((error, req, res, next) => {
 });
 
 app.listen(port, hostname, () => {
-    console.log(`O servidor da Elite Wolf está rodando em: http://${hostname}:${port}`);
+    console.log(`Servidor rodando no endereço ${hostname}:${port}`)
 });
+
+
